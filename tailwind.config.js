85e480cc9be33ddc21209d72712cd31f1012026a
Juzer Shakir <juzershakir.webdev@gmail.com>
2023-08-19 17:53:20 +0530

    custom screen-sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,14 @@
 module.exports = {
   content: ["./*.html"],
   theme: {
+    screens: {
+      sm: "480px",
+      sm2: "576px",
+      md: "768px",
+      lg: "976px",
+      lg2: "1250px",
+      xl: "1440px",
+    },
     extend: {},
   },
   plugins: [],
